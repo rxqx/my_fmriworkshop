1546cbf8ee3db7187b07e5201fa54962d4d8abc3
Rita.Lx <rludwig@uoregon.edu>
2017-12-12 11:16:22 -0500

    Beginning of prepro.py script

---
+++ prepro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 10:54:15 2017
+
+@author: rx
+"""
+
+def prepro(basedir):
+    print ('Data is in the path '+basedir)
+
+def main():
+    basedir='/Users/rx/Documents/Research/my_fmriworkshop/fmri_workshop/data'
+    prepro(basedir)
+
+main()
